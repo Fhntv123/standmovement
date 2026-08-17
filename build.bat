0ea884ade3c1f448e95290c980518cdabebdc068
unknown <sggyuk6@gmail.com>
2026-08-17 16:13:00 +0300

    local changes

--- build.bat
+++ build.bat
@@ -1,6 +1,5 @@
 @echo off
 chcp 65001 >nul
-setlocal enabledelayedexpansion
 
 echo ============================================================
 echo   yaw_cc build script
@@ -11,65 +10,17 @@ echo.
 
 :: === 1) Git sync ===
 echo [1/3] Syncing with remote...
-git pull --rebase
+git pull
 if errorlevel 1 (
-    echo [!] Pull failed!
-    pause
-    exit /b 1
-)
-git push
-if errorlevel 1 (
-    echo [!] Push failed!
-    pause
-    exit /b 1
+    echo [!] Pull failed! Trying without rebase...
+    git pull
 )
 echo.
 
 :: === 2) Build ===
 echo [2/3] Building Release x64...
+msbuild "ImGui DirectX 11 Kiero Hook.sln" /p:Configuration=Release /p:Platform=x64 /v:minimal
 
-set "MSBUILD="
-if exist "C:\Program Files\Microsoft Visual Studio\2022\Community\MSBuild\Current\Bin\MSBuild.exe" (
-    set "MSBUILD=C:\Program Files\Microsoft Visual Studio\2022\Community\MSBuild\Current\Bin\MSBuild.exe"
-)
-if exist "C:\Program Files\Microsoft Visual Studio\2022\Professional\MSBuild\Current\Bin\MSBuild.exe" (
-    set "MSBUILD=C:\Program Files\Microsoft Visual Studio\2022\Professional\MSBuild\Current\Bin\MSBuild.exe"
-)
-if exist "C:\Program Files (x86)\Microsoft Visual Studio\2019\Community\MSBuild\Current\Bin\MSBuild.exe" (
-    set "MSBUILD=C:\Program Files (x86)\Microsoft Visual Studio\2019\Community\MSBuild\Current\Bin\MSBuild.exe"
-)
-
-if "%MSBUILD%"=="" (
-    echo [!] MSBuild not found! Trying PATH...
-    msbuild "ImGui DirectX 11 Kiero Hook.sln" /p:Configuration=Release /p:Platform=x64 /v:minimal
-) else (
-    "%MSBUILD%" "ImGui DirectX 11 Kiero Hook.sln" /p:Configuration=Release /p:Platform=x64 /v:minimal
-)
-
-if errorlevel 1 (
-    echo [!] Build failed!
-    pause
-    exit /b 1
-)
 echo.
-
-:: === 3) Copy DLL ===
-echo [3/3] Copying DLL to cheat.dll...
-
-for /r "ImGui DirectX 11 Kiero Hook\x64\Release" %%f in (*.dll) do (
-    echo   Found: %%f
-    copy /Y "%%f" "cheat.dll"
-    echo   [OK] Copied to cheat.dll
-    goto :done
-)
-
-echo [ERROR] No DLL found!
-pause
-exit /b 1
-
-:done
-echo.
-echo ============================================================
-echo   BUILD COMPLETE
-echo ============================================================
+echo Build finished with code: %errorlevel%
 pause
